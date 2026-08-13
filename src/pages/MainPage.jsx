2d5c546b0f7063e39e3aeaebdf64ea8d7cd590eb
OlgaStarchenko <o9915530@gmail.com>
2026-08-13 20:04:55 +1000

    The logic for adding a new task has been implemented in the MainPage component. A "+" button has been added to the markup. The `hasInput` and `itemText` states have been introduced. Functions named `openAddItemModal` and `closeAddItemModal` have been implemented to manage the modal window for adding tasks. A POST request has been implemented to add a new task and update the task list.

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -13,14 +13,15 @@ export function MainPage() {
 	const [isAdding, setIsAdding] = useState(false);
 	const [refreshToDosFlag, setRefreshToDosFlag] = useState(false);
 	const [inputValue, setInputValue] = useState('');
-
+	const [hasInput, setHasInput] = useState(false);
+	const [itemText, setItemText] = useState('');
 	const [searchText, setSearchText] = useState('');
 	const [isSortedAlphabetically, setIsSortedAlphabetically] = useState(false);
 
 	const timerRef = useRef(null);
 
 	const refreshToDos = () => {
-		setRefreshToDosFlag(!refreshToDosFlag);
+		setRefreshToDosFlag((prev) => !prev);
 	};
 
 	useEffect(() => {
@@ -35,10 +36,64 @@ export function MainPage() {
 			.finally(() => setIsLoading(false));
 	}, [refreshToDosFlag]);
 
+	const requestAddToDoItem = () => {
+		fetch('http://localhost:3004/toDos', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json;charset=utf-8' },
+			body: JSON.stringify({
+				title: itemText,
+			}),
+		})
+			.then((rawResponse) => rawResponse.json())
+			.then((newItem) => {
+				refreshToDos();
+			})
+			.finally(() => {
+				setIsAdding(false);
+				setItemText('');
+			});
+	};
+
+	const openAddItemModal = () => {
+		setIsAdding(true);
+		setHasInput(true);
+	};
+
+	const closeAddItemModal = () => {
+		setIsAdding(false);
+		setItemText('');
+		setHasInput(false);
+	};
+
 	return (
 		<div className={styles.container}>
 			<h1 className={styles.title}>Todo List</h1>
-			<ToDoList toDoList={viewToDoList} />
+			{isLoading ? (
+				<div className={styles.loader}></div>
+			) : (
+				<ToDoList toDoList={viewToDoList} />
+			)}
+			<div className={styles.btn__add__container}>
+				<Button
+					text={'+'}
+					onClick={openAddItemModal}
+					variant="btn__open_add_modal"
+				/>
+			</div>
+			{isAdding && (
+				<Modal
+					acceptButtonText={'Add'}
+					cancelButtonText={'Cancel'}
+					cancelButtonOnClick={closeAddItemModal}
+					acceptButtonOnClick={requestAddToDoItem}
+					itemText={itemText}
+					setItemText={setItemText}
+					hasInput={hasInput}
+					disabledAcceptButton={itemText.trim() === ''}
+					placeholder={'Enter a new task...'}
+					isAdding={isAdding}
+				/>
+			)}
 		</div>
 	);
 }
